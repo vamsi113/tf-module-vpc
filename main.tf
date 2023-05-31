361266d5d44c735019deb3ff65c9a8ca0989008d
vamsi113 <krishultimate5@gmail.com>
2023-05-31 18:33:39 -0400

    change

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ resource "aws_vpc" "main" {
     tags       = local.vpc_tags
 }
 
-#module "additional_cidr_block" {
-#    for_each                = var.vpc
-#    source                  = "./additional_vpc_cidr"
-#    additional_cidr_block   = each.value.additional_cidr_block
-#    vpc_id                  = aws_vpc.main.id
-#}
+module "additional_cidr_block" {
+    for_each                = var.vpc
+    source                  = "./additional_vpc_cidr"
+    additional_cidr_block   = each.value.additional_cidr_block
+    vpc_id                  = [ for k,v in aws_vpc.main: v.id]
+}
 
 output "vpc" {
     value = aws_vpc.main
