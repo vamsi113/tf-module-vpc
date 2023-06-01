cbd9eec1bf66a6662506539dbe29193ee99db6e5
vamsi113 <krishultimate5@gmail.com>
2023-05-31 22:25:40 -0400

    change

--- subnets/main.tf
+++ subnets/main.tf
@@ -2,6 +2,6 @@ module "lm-subnets" {
   source        = "./lm-subnets"
   for_each      = var.subnets
   cidr_block    = each.value.cidr_block
-  vpc_id        = var.vpc_id
+  vpc_id        = var.vpc_id[0]
 
 }
