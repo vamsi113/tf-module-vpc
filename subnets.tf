e26ed3b28ef7f93ff0b4298d4c0200b9e5aca3a2
vamsi113 <krishultimate5@gmail.com>
2023-05-31 23:27:10 -0400

    change

--- subnets.tf
+++ subnets.tf
@@ -2,6 +2,7 @@ module "subnets" {
   for_each  = var.vpc
   source    = "./subnets"
   subnets   = each.value.subnets
-  vpc_id    = [ for k,v in aws_vpc.main: v.id]
+  vpc_id    = [ for k,v in aws_vpc.main: v.id ]
   env       = var.env
+  name      = each.value.name
 }
