375501866c9932f02afc54841e3084712b6f5735
vamsi113 <krishultimate5@gmail.com>
2023-06-02 18:21:04 -0400

    change

--- subnets/lm-subnets/main.tf
+++ subnets/lm-subnets/main.tf
@@ -5,3 +5,9 @@ resource "aws_subnet" "main" {
   tags       = local.subnet_tags
   availability_zone = element(var.subnet_availability_zones,count.index)
 }
+
+resource "aws_route_table_association" "aws_route_table_association" {
+  count          = length(aws_subnet.main)
+  subnet_id      = element(aws_subnet.main.*.id,count.index )
+  route_table_id = var.route_table_id
+}
