3083154d4e168ec027b7018bf42acb80e50db946
vamsi113 <krishultimate5@gmail.com>
2023-06-02 13:44:43 -0400

    change

--- subnets/main.tf
+++ subnets/main.tf
@@ -7,3 +7,13 @@ module "lm-subnets" {
   name          = each.value.name
   subnet_availability_zones = var.subnet_availability_zones
 }
+
+resource "aws_route_table" "route_table" {
+  for_each      = var.subnets
+  vpc_id        = var.vpc_id[0]
+  tags = {
+    Name = each.value.name
+  }
+
+
+}
