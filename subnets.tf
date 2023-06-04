c3e615237a399662d68c1ec7c30d0c6012df9b09
vamsi113 <krishultimate5@gmail.com>
2023-06-04 16:14:58 -0400

    change

--- subnets.tf
+++ subnets.tf
@@ -24,11 +24,14 @@ module "private_subnets" {
 
 }
 
-output "public_subnets" {
-  value = module.public_subnets
-}
+#output "public_subnets" {
+#  value = module.public_subnets
+#}
+#
+#output "private_subnets" {
+#  value = module.private_subnets
+#}
 
-output "private_subnets" {
-  value = module.private_subnets
+output "all_private_subnets" {
+  value = [for k,v in module.private_subnets: k]
 }
-
