3671e37f6b2d418baf06588bcc4bbba497e1d158
vamsi113 <krishultimate5@gmail.com>
2023-06-04 21:28:06 -0400

    change

--- gw.tf
+++ gw.tf
@@ -39,9 +39,9 @@ resource "aws_route" "internet_gateway_route_to_public_subnets" {
   gateway_id                = aws_internet_gateway.gw[0].id
 }
 
-#resource "aws_route" "nat_gateway_route_to_private_subnets" {
-#  count                     = length(local.private_route_tables)
-#  route_table_id            = element(local.private_route_tables, count.index)
-#  destination_cidr_block    = "0.0.0.0/0"
-#  gateway_id                = aws_nat_gateway.ngw[0].id
-#}
+resource "aws_route" "nat_gateway_route_to_private_subnets" {
+  count                     = length(local.private_route_tables)
+  route_table_id            = element(local.private_route_tables, count.index)
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_nat_gateway.ngw[0].id
+}
