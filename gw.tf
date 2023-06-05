7d83cffd048a8892f8a47098a53667c9cec34661
vamsi113 <krishultimate5@gmail.com>
2023-06-04 21:20:23 -0400

    change

--- gw.tf
+++ gw.tf
@@ -12,6 +12,7 @@ resource "aws_eip" "ip" {
 }
 
 resource "aws_nat_gateway" "ngw" {
+  count   = length(local.vpc_ids)
   allocation_id = aws_eip.ip.id
   subnet_id     = local.public_subnets_list[0]
   tags = {
@@ -31,9 +32,16 @@ locals {
 #  value = local.public_route_tables
 #}
 
-resource "aws_route" "internet_gateway_route_to_public_subnet" {
+resource "aws_route" "internet_gateway_route_to_public_subnets" {
   count                     = length(local.public_route_tables)
   route_table_id            = element(local.public_route_tables, count.index)
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.gw[0].id
 }
+
+#resource "aws_route" "nat_gateway_route_to_private_subnets" {
+#  count                     = length(local.private_route_tables)
+#  route_table_id            = element(local.private_route_tables, count.index)
+#  destination_cidr_block    = "0.0.0.0/0"
+#  gateway_id                = aws_nat_gateway.ngw[0].id
+#}
