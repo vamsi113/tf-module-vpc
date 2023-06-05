a2ac9a68156659f19b190717238ce23d5588831a
vamsi113 <krishultimate5@gmail.com>
2023-06-04 20:16:42 -0400

    change

--- gw.tf
+++ gw.tf
@@ -27,9 +27,9 @@ locals {
 }
 
 
-resource "aws_route" "internet_gateway_route_to_public_subnets" {
+resource "aws_route" "internet_gateway_route_to_public_subnet" {
   count                     = length(local.public_route_tables)
-  route_table_id            = element(local.public_route_tables,count.index)
+  route_table_id            = element(local.public_route_tables, count.index)
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.gw[0].id
 }
