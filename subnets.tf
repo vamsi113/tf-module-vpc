853d98d2b67d57ea897439fb027697216a15b10c
vamsi113 <krishultimate5@gmail.com>
2023-06-05 22:54:00 -0400

    change

--- subnets.tf
+++ subnets.tf
@@ -26,18 +26,19 @@
 
 
 
-#module "public_subnets" {
-#  for_each  = var.public_subnets
-#  source    = "./subnets"
-#  subnets   = each.value.public_subnets
-#  vpc_id    = [ for k,v in aws_vpc.main: v.id ]
-#  env       = var.env
-#  subnet_availability_zones = each.value.subnet_availability_zones
+module "public_subnets" {
+  for_each  = var.public_subnets
+  source    = "./subnets"
+  subnets   = each.value.cidr_block
+  vpc_id    = aws_vpc.main.id
+  env       = var.env
+  name = each.value.name
+  subnet_availability_zones = var.subnet_availability_zones
 #  peering_connection_id     = aws_vpc_peering_connection.management_vpc_to_env_vpc.*.id[0]
 #  management_vpc            = var.management_vpc
 #  internet_connection_id     = aws_internet_gateway.gw.*.id[0]
-#
-#}
+
+}
 
 module "private_subnets" {
   for_each  = var.private_subnets
