fc636410e0b2638cb820f84c1fc58cf2e2b7fd94
vamsi113 <krishultimate5@gmail.com>
2023-06-06 11:08:29 -0400

    change

--- peering.tf
+++ peering.tf
@@ -11,9 +11,9 @@ resource "aws_route" "route_from_default_management_route_table" {
   destination_cidr_block    = lookup(var.management_vpc,"default_vpc_cidr", null)
   vpc_peering_connection_id = aws_vpc_peering_connection.management_vpc_to_env_vpc.id
 }
-#
-#resource "aws_route" "route_to_default_management_route_table" {
-#  route_table_id            =  lookup(var.management_vpc,"route_table", null)
-#  destination_cidr_block    = var.cidr_block
-#  vpc_peering_connection_id = aws_vpc_peering_connection.management_vpc_to_env_vpc.id
-#}
+
+resource "aws_route" "route_to_default_management_route_table" {
+  route_table_id            =  lookup(var.management_vpc,"route_table", null)
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.management_vpc_to_env_vpc.id
+}
